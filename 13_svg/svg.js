a1705bee9c3c09176c1364f9e5feab6f5123f1e0
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2020-03-31 08:09:06 -0400

    13 copied

---
+++ 13_svg/svg.js
@@ -0,0 +1,56 @@
+// Yevgeniy Gorbachev and Moududur Rahman
+// SoftDev1 pd1/ pd9
+// K12 -- Connect the Dots
+// 2020-03-27
+
+const w3svg = "http://www.w3.org/2000/svg";
+const image = document.getElementById("vimage");
+const clearbutton = document.getElementById("clear");
+
+const radius = 10;
+
+function dot(x, y) {
+	let circle = document.createElementNS(w3svg, "circle");
+	circle.setAttribute("cx", x);
+	circle.setAttribute("cy", y);
+	circle.setAttribute("r", radius);
+	circle.setAttribute("fill", "red");
+	return circle;
+};
+
+function draw(e) {
+	let c = dot(e.offsetX, e.offsetY)
+	e.preventDefault()
+
+	if (e.target == image) {
+		image.appendChild(c);
+	}
+	c.addEventListener("click", changecolor);
+};
+
+
+function clear() {
+	image.innerHTML = "";
+};
+
+function changecolor(e) {
+	let c = e.target;
+	if (c.getAttribute("fill") == "red") {
+		c.setAttribute("fill", "blue");
+	}
+	else {
+		image.removeChild(c);
+		let newc = dot(
+			Math.floor(Math.random() * 501),
+			Math.floor(Math.random() * 501)
+		);
+
+		image.appendChild(newc);
+		e.preventDefault();
+		newc.addEventListener("click", changecolor);
+	}
+};
+
+
+image.addEventListener('click', draw);
+clearbutton.addEventListener('click', clear);
