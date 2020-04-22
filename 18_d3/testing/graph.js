ae02688528c2d2b6997015d32c63288fffb71a37
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2020-04-22 18:59:14 -0400

    animation working

--- 18_d3/testing/graph.js
+++ 18_d3/testing/graph.js
@@ -23,6 +23,16 @@ const GRAPH = d3.select("#chart-container").append("svg")
 	.attr("height", HEIGHT + MAR.left + MAR.right);
 // appends SVG
 
+const limits = {
+	x: d3.max(DATA, function(datum) {return datum[X_PARAM]}),
+	y: d3.max(DATA, function(datum) {return datum[Y_PARAM]}),
+}
+console.log(limits.x);
+console.log(limits.y);
+
+const scaled_x = function(datum) {return (WIDTH * ( datum[X_PARAM] / limits.x ));};
+const scaled_y = function(datum) {return (HEIGHT * ( 1 - datum[Y_PARAM] / limits.y ));};
+
 const generate_axes = function(graph, data) {
 	let x_max = d3.max(data, function(entry) {return entry[X_PARAM]});
 	let x_scale = d3.scaleLinear() // set x axis scale
@@ -43,16 +53,33 @@ const generate_axes = function(graph, data) {
 };
 
 const generate_points = function(graph, data) {
-	let limits = {
-		x: d3.max(data, function(datum) {return datum[X_PARAM]}),
-		y: d3.max(data, function(datum) {return datum[Y_PARAM]}),
-	}
-	graph.append("g").selectAll("dot").data(data).enter() // associates dots with data
-		.append("circle")
-		.attr("cx", function(datum) {return WIDTH * ( datum[X_PARAM] / limits.x ) + MAR.left;})
-		.attr("cy", function(datum) {return HEIGHT * ( 1 - datum[Y_PARAM] / limits.y ) + MAR.top;})
-		.attr("r", 3).style("fill", "black");
+
+	graph.append("g")
+		.attr("class", "lines")
+		.attr("transform", `translate(${MAR.left},${MAR.top})`)
+		.selectAll("dot").data(data).enter()
+		.append("line")
+		.attr("x1", scaled_x).attr("x2", scaled_x)
+		.attr("y1", scaled_y).attr("y2", scaled_y)
+		.style("stroke", "black").style("opacity", 1);
 };
+
+const animate_line = function(data) {
+	let ret = function(d, index) {
+		let line = d3.select(this);
+		if (data[index + 1]) {
+			scx = scaled_x( data[index + 1] )
+			scy = scaled_y( data[index + 1] )
+			line.transition()
+				.duration(500)
+				.delay(index * 500)
+				.attr("x2", scx)
+				.attr("y2", scy);
+		};
+	}
+	return ret;
+}
 
 generate_axes(GRAPH, DATA);
 generate_points(GRAPH, DATA);
+GRAPH.select(".lines").selectAll("line").each(animate_line(DATA));
